1d1463414bd667cf03e6e9fea1e95c5b8e02d1f2
Phong Mai <pmai@marinsoftware.com>
2014-02-02 11:05:34 +0800

    Added sequelize

---
+++ src/services/Sequelize.js
@@ -0,0 +1,54 @@
+var Context =       require('../Context'),
+    BaseService =   require(Context.__src('/services/BaseService')),
+    util =          require('util'),
+    q =             require('q'),
+    _ =             require('underscore'),
+    sequelize =     require('sequelize'),
+    sprintf =       require('sprintf-js').sprintf;
+
+/**
+ * Sequelize service
+ *
+ * @constructor
+ */
+var Sequelize = function() {
+    BaseService.call(this, 'Sequelize');
+
+    this._connection = null;
+};
+
+util.inherit(Sequelize, BaseService);
+
+/**
+ * Initialize the sequelize connection
+ *
+ * @param {Object} options - the options for the connection
+ * @param {Logger} Logger - the logger
+ */
+Sequelize.prototype.initialize = function(options, Logger) {
+    var deferred = q.defer();
+
+    options = options || {};
+    _.defaults(options, {
+        port:       3306,
+        host:       'localhost',
+        password:   null
+    });
+
+    // Initialize the connection
+    this._connection = new sequelize(options.db, options.username, options.password, options);
+
+    // Authenticate
+    this._connection.authenticate()
+        .complete(_.bind(function(err) {
+            if (err) {
+                this.error(Logger, sprintf('%s %s', Logger.formatString(options.serviceId), err.toString()));
+                deferred.reject(err);
+            } else {
+                this.info(Logger, sprintf('%s ready', Logger.formatString(options.serviceId)));
+                deferred.resolve();
+            }
+        }, this));
+};
+
+module.exports = Sequelize;
